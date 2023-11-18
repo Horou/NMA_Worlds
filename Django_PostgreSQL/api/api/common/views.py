13837368eae9c51d06b30f5fbc8fa9e3d4472fcf
Horou <nicolas.nemouthe@gmail.com>
2023-11-18 19:37:45 +0100

    Update for performance with prefetch_related

--- Django_PostgreSQL/api/api/common/views.py
+++ Django_PostgreSQL/api/api/common/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
-from .serializers import DeepSerializer
+from .serializers import DeepSerializer, FilesSerializer
 
 
 ########################################################################################################################
@@ -11,61 +11,74 @@ from .serializers import DeepSerializer
 
 
 class ReadOnlyDeepViewset(ReadOnlyModelViewSet):
-    serializer_class = DeepSerializer
     _viewsets = {}
-    _serializers = {}
-    _models = {}
-    _models_fields = {}
+    _mode = "Read"
 
     def __init_subclass__(cls, **kwargs):
-        if not cls._serializers:
-            cls._serializers = dict(DeepSerializer._serializers)
-        if cls.queryset:
-            nested_model = cls.queryset.model
-            serializer_name = nested_model.__name__
-            cls._models[nested_model.__name__] = nested_model
-            cls._models_fields[nested_model.__name__] = nested_model._meta.get_fields()
-            if serializer_name not in cls._serializers:
-
-                class ViewSetSerializer(DeepSerializer):
-                    class Meta:
-                        model = nested_model
-                        fields = '__all__'
-                        depth = 1
-
-                cls._serializers[serializer_name] = ViewSetSerializer
-            cls._viewsets[cls.__name__] = cls
         super().__init_subclass__(**kwargs)
+        if cls.queryset:
+            _model = cls.queryset.model
+            cls._viewsets[cls._mode + _model.__name__] = cls
+            cls._fields = {field.name for field in _model._meta.get_fields()}
+            cls.serializer_class = cls.get_serializer_class()
+            cls.depth = cls.serializer_class.Meta.depth if hasattr(cls.serializer_class.Meta, 'depth') else 0
+
+    @classmethod
+    def init_router(cls, router, models: list):
+        for model in models:
+            router.register(model.__name__, cls.get_viewsets(model))
+
+    @classmethod
+    def get_serializer_class(cls):
+        return DeepSerializer.get_serializer(cls.queryset.model, mode=cls._mode)
 
     def get_queryset(self):
-        data = self.request.query_params
-        fields_name = self._models_fields[self.queryset.model.__name__]
-        filter_by = {
-            field.name: data[field.name]
-            for field in fields_name
-            if field.name in data
-        }
-        queryset = self.queryset.filter(**filter_by) if filter_by else self.queryset
-        if "order_by" in data:
-            data_order_by = data["order_by"]
-            list_order_by = data_order_by if isinstance(data_order_by, list) else [data_order_by]
-            fields_names = {field.name for field in fields_name}
-            queryset = queryset.order_by(*[field for field in list_order_by if field.replace("-", "") in fields_names])
-        return queryset.select_subclasses()
+        params = self.request.query_params
+        depth = int(params.get("depth", self.depth))
+        serializer = self.get_serializer_class()
+        serializer.Meta.depth = depth
+        queryset = self.queryset.select_related(*serializer._select_related) if depth > 0 else self.queryset
+        queryset = queryset.prefetch_related(*serializer.get_prefetch_related()) if depth > 1 else queryset
+        filter_by = {name: params[name] for name in self._fields if name in params}
+        queryset = queryset.filter(**filter_by) if filter_by else queryset
+        if order_by := params.get("order_by", None):
+            list_order_by = order_by if isinstance(order_by, list) else [order_by]
+            queryset = queryset.order_by(*[field for field in list_order_by if field.replace("-", "") in self._fields])
+        return queryset
+
+    @classmethod
+    def get_viewsets(cls, _model, mode: str = ""):
+        if mode + _model.__name__ not in cls._viewsets:
+
+            class CommonViewSet(cls):
+                _mode = mode
+                queryset = _model.objects
+                depth = 0
+
+            CommonViewSet.__name__ = _model.__name__
+            CommonViewSet.__doc__ = f"View Set for the model: '{_model.__name__}' used for {mode if mode else 'Read and Write'}"
+
+        return cls._viewsets[mode + _model.__name__]
 
 
-class DeepViewset(ReadOnlyDeepViewset, ModelViewSet):
-    def get_serializer_class(self):
-        return self._serializers[self.queryset.model.__name__]
+class DeepViewSet(ReadOnlyDeepViewset, ModelViewSet):
+    _mode = ""
 
 
-class SpecialDeepViewset(DeepViewset):
+class UploadViewSet(DeepViewSet):
+    _mode = "Upload"
+
+    @classmethod
+    def get_serializer_class(cls):
+        return FilesSerializer.get_upload_serializer(cls.queryset.model)
+
+
+class SpecialDeepViewset(DeepViewSet):
+
     def create(self, request, *args, **kwargs):
-        block = {"TestModel4"}
-        serializer = self.get_serializer()
-        result, created = serializer.deep_create(request.data, block=block)
-        headers = self.get_success_headers(serializer.data)
-        return Response(result, status=status.HTTP_201_CREATED, headers=headers)
+        result, created = self.get_serializer().deep_create(request.data)
+        created["pk"] = result
+        return Response(created, status=status.HTTP_201_CREATED, headers=self.get_success_headers(created))
 
     def destroy(self, request, *args, **kwargs):
         serializer = self.get_serializer()
