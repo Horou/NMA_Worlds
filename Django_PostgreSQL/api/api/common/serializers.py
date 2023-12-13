890a6b8d9429a2295d0450d3bde26984222ee111
Horou <nicolas.nemouthe@gmail.com>
2023-12-13 19:52:29 +0100

    Still not perfect

--- Django_PostgreSQL/api/api/common/serializers.py
+++ Django_PostgreSQL/api/api/common/serializers.py
@@ -24,7 +24,7 @@ class DeepSerializer(serializers.ModelSerializer):
             cls._serializers[cls._mode + model.__name__] = cls
             cls._nested_models = cls.build_nested_models(model)
             cls._prefetch_related = [p[2:] for p in cls.build_prefetch_related(model, [model])]
-            cls.nested_prefetch = cls._prefetch_related
+            cls.prefetch_related = cls.to_prefetch_related()
             cls.Meta.read_only_fields = tuple(model_meta.get_field_info(model).reverse_relations)
 
     @classmethod
@@ -48,7 +48,7 @@ class DeepSerializer(serializers.ModelSerializer):
         return prefetch_related
 
     @classmethod
-    def get_prefetch_related(cls, excludes: list[str] = []):
+    def to_prefetch_related(cls, excludes: list[str] = []):
         return [
             prefetch_related
             for prefetch_related in cls._prefetch_related
@@ -59,7 +59,7 @@ class DeepSerializer(serializers.ModelSerializer):
     @classmethod
     def get_nested_prefetch(cls, field_name):
         nested_prefetch = []
-        for prefetch in cls.nested_prefetch:
+        for prefetch in cls.prefetch_related:
             child_prefetch = prefetch.split('__')
             if 1 < len(child_prefetch) < cls.Meta.depth + 2 and child_prefetch[0] == field_name:
                 nested_prefetch.append("__".join(child_prefetch[1:]))
@@ -70,12 +70,12 @@ class DeepSerializer(serializers.ModelSerializer):
                 [model_info.pk.name] +
                 list(declared_fields) +
                 list(model_info.fields) +
-                list(set(field.split('__')[0] for field in self.nested_prefetch))
+                list(set(field.split('__')[0] for field in self.prefetch_related))
         )
 
     def build_nested_field(self, field_name, relation_info, nested_depth):
         serializer = self.get_serializer(relation_info.related_model, mode=f"Read{self.Meta.model.__name__}Nested")
-        serializer.nested_prefetch = self.get_nested_prefetch(field_name)
+        serializer.prefetch_related = self.get_nested_prefetch(field_name)
         serializer.Meta.depth = nested_depth - 1
         return serializer, get_nested_relation_kwargs(relation_info)
 
@@ -99,12 +99,11 @@ class DeepSerializer(serializers.ModelSerializer):
                 for (data, rep), result in zip(dicts, serializer.deep_list_travel([d[field_name] for d, _ in dicts])):
                     data[field_name], rep[field_name] = result
             elif lists := [d_n for d_n in to_create if isinstance(d_n[0].get(field_name, None), list)]:
-                flatten_result = serializer.deep_list_travel([i for d, _ in lists for i in d[field_name]])
-                start = 0
+                flatten_results = serializer.deep_list_travel([i for d, _ in lists for i in d[field_name]])
                 for data, nested in lists:
                     if length := len(data[field_name]):
-                        data[field_name], nested[field_name] = map(list, zip(*flatten_result[start:start + length]))
-                        start += length
+                        data[field_name], nested[field_name] = map(list, zip(*flatten_results[:length]))
+                        flatten_results = flatten_results[length:]
         return self.bulk_update_or_create(data_and_nested_list)
 
     def update_or_create(self, data: dict, nested: dict, instances: dict = None):
@@ -120,7 +119,7 @@ class DeepSerializer(serializers.ModelSerializer):
         pks_and_representations, created = [], {}
         pk_name = self.Meta.model._meta.pk.name
         found_pks = set(d[pk_name] for d, _ in data_and_nested if pk_name in d)
-        instances = self.Meta.model.objects.prefetch_related(*self.get_prefetch_related()).in_bulk(found_pks)
+        instances = self.Meta.model.objects.prefetch_related(*self.to_prefetch_related()).in_bulk(found_pks)
         for data, nested in data_and_nested:
             if isinstance(data, dict):
                 found_pk = data.get(pk_name, None)
@@ -136,8 +135,7 @@ class DeepSerializer(serializers.ModelSerializer):
                 pks_and_representations.append((data, data))
         return pks_and_representations
 
-    def deep_create(self, data: dict | list):
-        representation = type(data)()
+    def deep_create(self, data: dict | list, verbose: bool = True):
         try:
             with atomic():
                 serializer = self.get_serializer(self.Meta.model, mode="Nested")(context=self.context)
@@ -151,7 +149,7 @@ class DeepSerializer(serializers.ModelSerializer):
                         raise ValidationError(errors)
         except ValidationError as e:
             return e.detail
-        return representation
+        return representation if verbose else primary_key
 
     @classmethod
     def get_serializer(cls, _model, mode: str = ""):
